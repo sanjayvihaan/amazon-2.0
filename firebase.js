9577dd40044ea33ff21538eaa0339fe70fa13a33
V Sanjay <sanjayvihaan111@gmail.com>
2021-12-27 00:18:28 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDUsmFtONiugKltZmJ5iuHZHypCZUi0MOk",
+    authDomain: "clone-71db8.firebaseapp.com",
+    projectId: "clone-71db8",
+    storageBucket: "clone-71db8.appspot.com",
+    messagingSenderId: "942483461114",
+    appId: "1:942483461114:web:e1e2ece19e34c205d84a43"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  var db = firebase.firestore();
